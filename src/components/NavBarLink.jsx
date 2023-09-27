ef34549df9def45762b86c985c3f0a1bb4be4aa0
Dimon1511 <dimatis0911@gmail.com>
2023-09-27 19:09:45 +0300

    first commit

--- src/components/NavBarLink.jsx
+++ src/components/NavBarLink.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
-import styles from "./NavbarLink.module.css";
+import styles from "./NavBarLink.module.css";
 
 function NavBarLink({ to, children }) {
   return (
